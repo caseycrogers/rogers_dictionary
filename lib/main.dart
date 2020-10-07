7e161409bfc6bc2693a5da70ff24630bb76b8cc2
Casey Rogers <caseycrogers@berkeley.edu>
2020-10-07 13:18:11 -0700

    added Firebase and Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rogers_dictionary/entry_list.dart';
 
-void main() => runApp(MyApp());
+import 'article_database/entry_database.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Dictionary',
+      home: ArticleList(),
     );
   }
 }
