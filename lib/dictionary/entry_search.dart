a712bcb39a6882908a20cf31903fd3ca30f2a210
Casey Rogers <caseycrogers@berkeley.edu>
2020-11-30 20:18:59 -0800

    Moved entrylist state into DictionaryPageModel

--- lib/dictionary/entry_search.dart
+++ lib/dictionary/entry_search.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rogers_dictionary/dictionary/search_bar.dart';
 import 'package:rogers_dictionary/models/dictionary_page_model.dart';
-import 'package:rogers_dictionary/models/search_string_model.dart';
 
 import 'entry_list.dart';
 
@@ -11,22 +10,26 @@ class EntrySearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var initialModel = DictionaryPageModel.of(context).searchStringModel;
+    DictionaryPageModel pageModel = DictionaryPageModel.of(context);
+    SearchStringModel initialModel = pageModel.searchStringModel;
     return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: ChangeNotifierProvider<SearchStringModel>.value(
-        value: initialModel,
-        builder: (context, child) {
-          return Column(children: [
-              SearchBar(initialModel.value),
-              Expanded(
-                child: Consumer<SearchStringModel> (
-                  builder: (context, searchStringModel, child) => EntryList(searchStringModel.value),
-                ),
-              ),
-            ]);
-        },
-      ),
+      decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          boxShadow: [BoxShadow(
+            color: Colors.grey.withOpacity(.5),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          )]),
+      child: Column(children: [
+        SearchBar(),
+        Expanded(
+          child: ChangeNotifierProvider.value(
+            value: initialModel,
+            child: EntryList(),
+          ),
+        ),
+      ]),
     );
   }
 }
--- lib/models/search_string_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SearchStringModel extends ValueNotifier<String> {
-  SearchStringModel() : super('');
-}
