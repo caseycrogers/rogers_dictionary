b27590970869e0b16d589ce50c4d6b4e2d9e3d40
caseycrogers <caseycrogers@berkeley.edu>
2021-06-28 11:42:21 -0700

    finished dealing with apple bullshit

--- lib/widgets/buttons/translation_mode_selector.dart
+++ lib/widgets/buttons/translation_mode_selector.dart
@@ -11,56 +11,64 @@ import 'package:rogers_dictionary/models/translation_page_model.dart';
 class TranslationModeSelector extends StatelessWidget {
   const TranslationModeSelector({Key? key}) : super(key: key);
 
-  static const double _buttonWidth = 110;
-  static const double _buttonHeight = 50;
-  static const double _buttonSpacing = 10;
+  static const _englishKey = ValueKey('spanishButton');
+  static const _spanishKey = ValueKey('englishButton');
 
   @override
   Widget build(BuildContext context) {
     final DictionaryPageModel pageModel = DictionaryPageModel.of(context);
     return ValueListenableBuilder<double>(
       valueListenable: pageModel.pageOffset,
-      builder: (context, pageOffset, _) => Container(
-        height: _buttonHeight,
-        width: 2 * _buttonWidth + _buttonSpacing,
-        child: Stack(
-          children: [
-            AnimatedPositioned(
-              left: lerpDouble(
-                0,
-                _buttonWidth + _buttonSpacing,
-                pageOffset,
-              )!,
-              child: Container(
-                width: _buttonWidth,
-                height: _buttonHeight,
-                decoration: BoxDecoration(
-                  color: Colors.black38,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-              ),
-              duration: const Duration(milliseconds: 100),
-            ),
-            Row(
+      builder: (context, pageOffset, _) =>
+          Container(
+            height: _Button._buttonHeight,
+            child: Stack(
               children: [
-                _button(context, TranslationMode.English),
-                const SizedBox(width: _buttonSpacing),
-                _button(context, TranslationMode.Spanish),
+                AnimatedPositioned(
+                  left: lerpDouble(
+                    0,
+                    100 + _Button._buttonSpacing,
+                    pageOffset,
+                  )!,
+                  child: Container(
+                    width: 100,
+                    height: _Button._buttonHeight,
+                    decoration: BoxDecoration(
+                      color: Colors.black38,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  duration: const Duration(milliseconds: 100),
+                ),
+                Row(
+                  children: const [
+                    _Button(TranslationMode.English, key: _englishKey),
+                    SizedBox(width: _Button._buttonSpacing),
+                    _Button(TranslationMode.Spanish, key: _spanishKey),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
     );
   }
+}
 
-  Widget _button(BuildContext context, TranslationMode mode) {
+class _Button extends StatelessWidget {
+  const _Button(this.mode, {Key? key}) : super(key: key);
+
+  static const double _buttonHeight = 50;
+  static const double _buttonSpacing = 10;
+
+  final TranslationMode mode;
+
+  @override
+  Widget build(BuildContext context) {
     final DictionaryPageModel dictionaryModel = DictionaryPageModel.of(context);
     final bool isSelected =
         dictionaryModel.translationPageModel.value.translationMode == mode;
     return Container(
       height: _buttonHeight,
-      width: _buttonWidth,
       child: TextButton(
         style: ButtonStyle(
           overlayColor: MaterialStateProperty.all(Colors.black38),
@@ -75,10 +83,14 @@ class TranslationModeSelector extends StatelessWidget {
             mode == TranslationMode.English
                 ? i18n.english.cap.get(context)
                 : i18n.spanish.cap.get(context),
-            style: Theme.of(context).textTheme.headline1!.copyWith(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
+            style: Theme
+                .of(context)
+                .textTheme
+                .headline1!
+                .copyWith(
+              color: Colors.white,
+              fontSize: 24,
+            ),
           ),
         ),
       ),
